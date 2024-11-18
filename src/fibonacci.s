0c00b41e7e8c282fe675ac766d29c661346aa1f7
RobinSalihAxelsson <axelsson.robin@outlook.com>
2024-11-18 16:59:32 +0100

    feat: print to 377

--- src/fibonacci.s
+++ src/fibonacci.s
@@ -3,9 +3,10 @@ global  _start
 section .text
 
 _start:
+    mov     rbp, rsp      ; get a base stack pointer
     push    0x30
     mov     rdi, rsp      ; func arg 1
-    call print_char       ; print "0"
+    call print_char_irdip ; print "0"
     pop     rax           ; clean the stack
 
     call print_newline
@@ -16,27 +17,32 @@ _start:
 
 push_dec_loop:
     xor     rdx, rdx
-    mov     rcx, 10     ; rcx = divisior
-    idiv    rcx         ; rax = qoutient
-    push    rdx         ; save reminder on stack
-    cmp     rdx, 0
-    jne     push_dec_loop
+    mov     rcx, 10       ; divide by 10 to get the last decimal digit in reminder
+    idiv    rcx
+    ;rax = rax/rcx
+    ;rdx = rax/rcx
+    cmp     rdx, 0        ; if reminder is 0 we have no more digits
+    jle print_dec_loop
+
+    push    rdx           ; save our decimal digit on stack
+    jmp    push_dec_loop
 
 print_dec_loop:
-    add     byte [rsp], 0x30    ; convert decimal to ASCII equivalent
+    add     qword[rsp], 0x30    ; convert decimal to ASCII equivalent
     mov     rdi, rsp
-    call print_char
+    call print_char_irdip
     
-    pop     rax
-    cmp     byte [rsp], 0
-    jnz     print_dec_loop
+    pop     rax             ; pop off the printed digit from the stack
+    cmp     rsp, rbp        ; if stack pointer is back to base
+    jne     print_dec_loop
     call print_newline
 
-add_fibonacci:
+
+add_fibonacci_r12_r13:
     add     r12, r13
     xchg    r12, r13
-    mov     rax, r13     ; rax = dividend
-    cmp     r13, 100
+    mov     rax, r13
+    cmp     r13, 121393
     jle     push_dec_loop
 
 exit:
@@ -47,11 +53,11 @@ exit:
 print_newline:
     push    0xa
     mov     rdi, rsp
-    call print_char
-    pop     rax         ; clean the stack
+    call print_char_irdip
+    pop     rax         ; restore the stack
     ret
 
-print_char:             ; rdi - pointer to ascii bytes
+print_char_irdip:       ; input arg rdi - pointer to ascii bytes
     mov     rdx, 1
     mov     rsi, rdi
     mov     rdi, 1
