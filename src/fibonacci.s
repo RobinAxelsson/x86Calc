b97ee7ba8b4b44ea566d29e5779a2d9ec8d4a303
RobinSalihAxelsson <axelsson.robin@outlook.com>
2024-11-18 13:28:19 +0100

    printing unlimited stack addition fibo

--- src/fibonacci.s
+++ src/fibonacci.s
@@ -13,34 +13,38 @@ _start:
 
     mov     r12, 0        ; r12, callee saved register, will hold our trailing fibonacci number
     mov     r13, 1        ; r13, callee saved register, will hold the higher fibonacci number
-    mov     rcx, 5        ; loop counter
+    mov     rax, r13      ; rax = dividend
 
-loopFib:
-    push    rcx
+push_dec_loop:
+    xor     rdx, rdx
+    mov     rcx, 10     ; rcx = divisior
+    idiv    rcx         ; rax = qoutient
+    push    rdx         ; save reminder on stack
+    cmp     rdx, 0
+    jne     push_dec_loop
 
-    call print_fib
+print_dec_loop:
+    add     byte [rsp], 0x30    ; convert decimal to ASCII equivalent
+    mov     rdi, rsp
+    mov     rsi, 1
+    call print_str
+    
+    pop     rax
+    cmp     byte [rsp], 0
+    jnz     print_dec_loop
     call print_newline
 
+add_fibonacci:
     add     r12, r13
     xchg    r12, r13
-
-    pop     rcx
-    loop loopFib
+    mov     rax, r13     ; rax = dividend
+    cmp     r13, 100
+    jle     push_dec_loop
 
 exit:
     mov     rax, 60
     mov     rdi, 0
     syscall
-
-print_fib:
-    mov     rbx, r13
-    add     rbx, 0x30     ; convert number to ASCII equivalent
-    push    rbx
-    mov     rdi, rsp
-    mov     rsi, 1
-    call print_str
-    pop     rax
-    ret
 
 print_newline:
     push 0xa
@@ -49,7 +53,6 @@ print_newline:
     call print_str
     pop     rax
     ret
-
 
 print_str:             ; rdi - pointer to ascii bytes, rsi - i bytes to print
     mov     rdx, rsi
