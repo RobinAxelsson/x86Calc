23ce657e29c6b6d1cd50373fac92c4706d8bb820
RobinSalihAxelsson <axelsson.robin@outlook.com>
2024-11-21 21:43:07 +0100

    version 2 generated

--- src/print-args.s
+++ src/print-args.s
@@ -1,48 +1,38 @@
-global  _start
-
 section .data
-    arg: db      "length-args: "
-    argl: db     13
-
+section .bss
 section .text
 
+global _start
+
 _start:
-    mov     rdx, [argl]      ; length of string
-    mov     rsi, arg
-    call print_str_rsiptr_rdxl
+    ; Clear registers
+    xor     rdi, rdi          ; Clear RDI (for environment pointer)
+    mov     r8, [rsp + 8]     ; Load address of the first environment variable
 
-print_arg_length:
-    add     rdi, 0x30   ; convert to ascii
-    push    rdi
-    mov     rdx, 2      ; length of string
-    mov     rsi, rsp
-    call print_str_rsiptr_rdxl
-    pop     rax         ; pop off the ascii char
+print_arg:
+    ; Calculate the length of the string (environment variable)
+    xor     rdx, rdx          ; Clear rdx (used for string length)
+    mov     rsi, r8         ; Load address of the environment variable string
     
-    call print_newline
+    find_null_terminator:
+        mov     al, byte [rsi + rdx]  ; Load the byte at rsi + rdx
+        cmp     al, 0x00       ; Check if it's the null terminator
+        je      null_found     ; Jump if null terminator is found
+        inc     rdx            ; Otherwise, increment length counter
+        jmp     find_null_terminator
 
-print_arg_1:
-    pop     rbp         ; save the stack
-    mov     rdx, 16      ; length of string
-    mov     rsi, rsp
-    call print_str_rsiptr_rdxl
-    call print_newline
+    null_found:
+        mov     rdx, rdx       ; Set rdx to the calculated length
+
+    call    print_str_rsiptr_rdxl
 
 exit:
-    mov     rax, 60
-    mov     rdi, 0
-    syscall
-
-print_newline:
-    push 0xa
-    mov rsi, rsp
-    mov rdx, 1
-    call print_str_rsiptr_rdxl
-    pop rax
-    ret
+    mov     rax, 60           ; Exit syscall number
+    mov     rdi, 0            ; Exit status 0
+    syscall                   ; Make the syscall
 
 print_str_rsiptr_rdxl:
-    mov     rdi, 1
-    mov     rax, 1
-    syscall
+    mov     rdi, 1            ; File descriptor (stdout)
+    mov     rax, 1            ; Syscall number for write
+    syscall                   ; Make the syscall
     ret
