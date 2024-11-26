d75f03fec901efd48c01850e5eb5db6a4afe1487
RobinSalihAxelsson <axelsson.robin@outlook.com>
2024-11-26 12:19:58 +0100

    print all args

--- src/print-args.s
+++ src/print-args.s
@@ -13,25 +13,33 @@ print_argv:
     add     rbx, 0xA30        ; add a new line and convert to ascii
     push    rbx
     mov     rsi, rsp
-    mov     rdx, 2
+    mov     rdx, 2            ; only works for args that are less then 10
     call print_str_rsiptr_rdxl
     pop     rax
 
+    xor     rbx, rbx
+
 print_arg:
-    mov     rsi, [rsp + 8]     ; Load address of the first environment variable
-    xor     rdx, rdx          ; Clear rdx (used for string length)
+    inc     rbx
+    mov     rsi, [rsp + (8*rbx)]     ; Load address of the first environment variable
+    xor     rdx, rdx                 ; Clear rdx (used for string length)
     
     find_null_terminator:
         mov     al, byte [rsi + rdx]  ; Load the byte at rsi + rdx
-        cmp     al, 0x00       ; Check if it's the null terminator
-        je      null_found     ; Jump if null terminator is found
-        inc     rdx            ; Otherwise, increment length counter
+        cmp     al, 0x00              ; Check if it's the null terminator
+        je      null_found            ; Jump if null terminator is found
+        inc     rdx                   ; Otherwise, increment length counter
         jmp     find_null_terminator
 
     null_found:
-        mov     rdx, rdx       ; Set rdx to the calculated length
 
-    call    print_str_rsiptr_rdxl
+        push    rbx
+        call    print_str_rsiptr_rdxl
+        call    print_newline
+
+        pop     rbx
+        cmp     rbx, [rsp]
+        jl      print_arg     ; if all args are printed
 
 exit:
     mov     rax, 60           ; Exit syscall number
@@ -42,4 +50,14 @@ print_str_rsiptr_rdxl:
     mov     rdi, 1            ; File descriptor (stdout)
     mov     rax, 1            ; Syscall number for write
     syscall                   ; Make the syscall
+    ret
+
+print_newline:
+    push    0xA
+    mov     rdx, 1
+    mov     rsi, rsp
+    mov     rdi, 1            ; File descriptor (stdout)
+    mov     rax, 1            ; Syscall number for write
+    syscall                   ; Make the syscall
+    pop     rax
     ret
