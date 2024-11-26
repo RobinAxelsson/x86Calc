ee0e718dc4b6fca30400a66b4a956e5b5a6236af
RobinSalihAxelsson <axelsson.robin@outlook.com>
2024-11-26 16:50:32 +0100

    feat: added string equals

--- src/equals.s
+++ src/equals.s
@@ -1,7 +1,9 @@
 global int_equals
+
 extern int_equals
 extern ref_equals
 extern deref_equals
+extern str_equals
 
 section .data
 
@@ -13,14 +15,27 @@ int_equals:
     sete    al
     ret
 
-ref_equals:
+ref_equals: ; qword 64 bit ptr
     xor     rax, rax
     cmp     rsi, rdi
     sete    al
     ret
 
-deref_equals:
+deref_equals: ; dword int
     xor     rax, rax
-    cmp     rsi, rdi
+    mov     esi, [rsi]  ; memory addresses are 8 bytes, but int is 4 bytes
+    mov     edi, [rdi]
+    cmp     esi, edi
     sete    al
     ret
+
+str_equals:
+    xor     rax, rax
+    mov     cx, 10000
+    ; iterates until end of string or end of cx
+    repe cmpsb ;repz cmps BYTE PTR ds:[rsi], BYTE PTR es:[rdi]
+    sete    al
+    
+    ret
+
+; bytes_equals:
--- test/equals_tests.c
+++ test/equals_tests.c
@@ -3,12 +3,20 @@
 
 extern int int_equals(int rdi, int rsi);
 extern int ref_equals(int* rdi, int* rsi);
+extern int deref_equals(int* rdi, int* rsi);
+extern int str_equals(char* a, char* b);
+
 char *apply_result(int boolean_result);
 
 void int_equals_55_1();
 void int_equals_56_0();
+void int_equals_intMax_1();
 void ref_equals_ptrptr_0();
 void ref_equals_ptrptr_1();
+void deref_equals_ptrptr_1();
+void str_equals_HelloHello_1();
+void str_equals_HelloHalla_0();
+void str_equals_HelloMr_0();
 
 int passed = 0;
 int tests = 0;
@@ -17,9 +25,16 @@ int main() {
 
 int_equals_55_1();
 int_equals_56_0();
+int_equals_intMax_1();
 
 ref_equals_ptrptr_0();
 ref_equals_ptrptr_1();
+
+deref_equals_ptrptr_1();
+
+str_equals_HelloHello_1();
+str_equals_HelloHalla_0();
+str_equals_HelloMr_0();
 
 //Summary
     printf("---------------------------------------\n");
@@ -34,7 +49,7 @@ void int_equals_55_1()
     int exp = 1;
     char *fail = apply_result(exp == res);
 
-    printf("%d, int_equals - expected: %d result: %d %s\n", tests, exp, res, fail);
+    printf("%d, int_equals_55_1 - expected: %d result: %d %s\n", tests, exp, res, fail);
 }
 
 void int_equals_56_0()
@@ -43,20 +58,29 @@ void int_equals_56_0()
     int exp = 0;
     char *fail = apply_result(exp == res);
 
-    printf("%d, int_equals - expected: %d result: %d %s\n", tests, exp, res, fail);
+    printf("%d, int_equals_56_0 - expected: %d result: %d %s\n", tests, exp, res, fail);
 }
 
-void ref_equals_ptrptr_0()
+void int_equals_intMax_1()
+{
+    int res = int_equals(0x7FFFFFFF, 0x7FFFFFFF);
+    int exp = 1;
+    char *fail = apply_result(exp == res);
+
+    printf("%d, int_equals_intMax_1 - expected: %d result: %d %s\n", tests, exp, res, fail);
+}
+
+void ref_equals_ptrptr_1()
 {
     int a = 5;
     int res = ref_equals((int*)&a, (int*)&a);
     int exp = 1;
     char *fail = apply_result(exp == res);
 
-    printf("%d, ref_equals - expected: %d result: %d %s\n", tests, exp, res, fail);
+    printf("%d, ref_equals_ptrptr_1 - expected: %d result: %d %s\n", tests, exp, res, fail);
 }
 
-void ref_equals_ptrptr_1()
+void ref_equals_ptrptr_0()
 {
     int a = 5;
     int b = 5;
@@ -64,13 +88,60 @@ void ref_equals_ptrptr_1()
     int exp = 0;
     char *fail = apply_result(exp == res);
 
-    printf("%d, ref_equals - expected: %d result: %d %s a: %d, b: %d\n", tests, exp, res, fail, a, b);
+    printf("%d, ref_equals_ptrptr_0 - expected: %d result: %d %s\n", tests, exp, res, fail);
 }
 
-char* apply_result(int boolean_result){
+void deref_equals_ptrptr_1()
+{
+    int a = 5;
+    int b = 5;
+    int res = deref_equals((int*)&a, (int*)&b);
+    int exp = 1;
+    char *fail = apply_result(exp == res);
+
+    printf("%d, deref_equals_ptrptr_1 - expected: %d result: %d %s\n", tests, exp, res, fail);
+}
+
+void str_equals_HelloHello_1()
+{
+    char* a = "Hello";
+    char* b = "Hello";
+
+    int res = str_equals(a, b);
+    int exp = 1;
+    char *fail = apply_result(exp == res);
+
+    printf("%d, str_equals_HelloHello_1 - expected: %d result: %d %s\n", tests, exp, res, fail);
+}
+
+void str_equals_HelloHalla_0()
+{
+    char* a = "Hello";
+    char* b = "Halla";
+
+    int res = str_equals(a, b);
+    int exp = 0;
+    char *fail = apply_result(exp == res);
+
+    printf("%d, str_equals_HelloHalla_0 - expected: %d result: %d %s\n", tests, exp, res, fail);
+}
+
+void str_equals_HelloMr_0()
+{
+    char* a = "Hello mr";
+    char* b = "Hello ";
+
+    int res = str_equals(a, b);
+    int exp = 0;
+    char *fail = apply_result(exp == res);
+
+    printf("%d, str_equals_HelloMr_0 - expected: %d result: %d %s\n", tests, exp, res, fail);
+}
+
+char* apply_result(int boolean_result)
+{
     tests++;
     char* fail = "";
-    printf("bool: %d\n", boolean_result);
 
     if(!boolean_result){
         fail = " - FAILED";
