58b55228e4063a9f86a98a4af3bab3e79be48576
RobinSalihAxelsson <axelsson.robin@outlook.com>
2024-11-30 13:45:56 +0100

    test: add calc.s test script sh

---
+++ src/calc.s
@@ -0,0 +1,13 @@
+global _start
+
+section .data
+section .text
+
+
+_start:
+ 
+
+error_exit:
+    mov     rax, 60
+    mov     rdi, 1
+    syscall
---
+++ test/calc_tests.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# prefer to send it to std out (echo)
+
+#-------BUILD--------
+
+mkdir -p output
+nasm -f elf64 ./src/calc.s -o ./output/calc.o
+ld ./output/calc.o -o ./output/calc
+
+#-----TEST-SETUP-----
+
+tests=0
+passed=0
+expression=""
+exp=111 # to avoid false positives
+res=999
+
+run_exit_test(){
+    eval "$1" # test-expression
+    res="$?"
+    exp="$2"
+
+    ((tests++))
+    if [ $exp -eq $res ]; then
+        ((passed++))   # (()) is for aritmetics, no $ needed
+    else 
+        fail="<------ FAILED"
+    fi
+
+    echo "$1 res=$res exp=$exp $fail"
+}
+
+#-------TESTS--------
+
+# wrong input expect fail
+# $? is return value for any function or command (exit status)
+run_exit_test "./output/calc" 1
+run_exit_test "./output/calc 1+1" 0
+
+#-------SUMMARY------
+echo "--------------------------"
+echo "$passed"/"$tests" passed
+
+if [ $passed -ne $tests ]; then
+    exit 1
+fi
